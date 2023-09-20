930d629bce00291750edef7965551aeeb8aab0a7
Vini27Br <viniciusouza0@gmail.com>
2023-09-20 08:58:35 -0300

    coisas 1

--- src/pages/Catalógo/index.tsx
+++ src/pages/Catalógo/index.tsx
@@ -1,16 +1,26 @@
 import styles from './Catalogo.module.scss'
-
-
+import Logo from '../../assets/Logo.png'
 
 export default function Catalogo(){
 
     return (
+
+        <main>
+            
+            <nav className={styles.menu}>
+                
+            </nav>
+
        <header className={styles.header}>
 
            <div className={styles.header__text}>
                "A melhor pizzaria do reino dos cogumelos"
            </div>
        </header>
+
+       </main>
+
+
     )
 
 
