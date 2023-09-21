f79bb18f3ed12bdf334756c9910bc24f36ba1795
Vini27Br <viniciusouza0@gmail.com>
2023-09-21 09:02:12 -0300

    coisas 3

--- src/pages/Catalógo/index.tsx
+++ src/pages/Catalógo/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Catalogo.module.scss'
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/Logo2.png'
+import Banner from '../../assets/Catalogo/banner.png'
 
 export default function Catalogo(){
 
@@ -8,12 +9,13 @@ export default function Catalogo(){
         <main>
             
             <nav className={styles.menu}>
-                
+            <img src={ Logo } alt='Logo' className='img'/>
             </nav>
 
        <header className={styles.header}>
 
            <div className={styles.header__text}>
+               <img src={Banner} alt="" />
                "A melhor pizzaria do reino dos cogumelos"
            </div>
        </header>
