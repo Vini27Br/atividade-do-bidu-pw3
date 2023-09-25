e551fff7c18514f1697aaa25de1f445699d62567
Vini27Br <viniciusouza0@gmail.com>
2023-09-25 10:56:01 -0300

    coisas 3

---
+++ src/pages/Catalógo/filtros/index.tsx
@@ -0,0 +1,10 @@
+import  Styles  from './Filtros.module.scss'
+import filtros from './filtros.json'
+
+type Iopcao = typeof filtros[];
+
+export default function Filtros(opcao: Iopcao){
+
+return(<div></div>)
+
+}
